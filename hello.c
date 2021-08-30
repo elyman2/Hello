37ec33f8cca0f75e86d97f2b7e9474e0a0a8ee95
Ellie Lyman <elyman2@huskers.unl.edu>
2021-08-30 18:48:50 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Ellie Lyman
+ * Date: 2021 (8-)
+ * 
+ * Hack_1 
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+   
+    printf("Ellie Lyman!\n");
+    
+    return 0;
+    
+}
