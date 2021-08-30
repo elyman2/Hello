211a2d7614c48e5ce8e6109478218fe78333dafa
Ellie Lyman <elyman2@huskers.unl.edu>
2021-08-30 19:55:37 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,16 +1,17 @@
 /**
  * Author: Ellie Lyman
- * Date: 2021 (8-)
- * 
- * Hack_1 
+ * Date: 2021 (8-30)
+ *
+ * Hack_1
  */
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-   
-    printf("Ellie Lyman!\n");
-    
+
+    printf("Ellie Lyman!!\n");
+    printf("Undecided\n");
+
     return 0;
-    
+
 }
